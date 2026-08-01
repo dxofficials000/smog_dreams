ffdcc1e1a1445b90d9db13825895252ceee62f13
dxofficials000 <aliezedavid000@gmail.com>
2026-08-01 10:58:31 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,122 @@
+const smogProducts = [
+    {
+        id: 1,
+        name: "Sorapad 5G Tablet (S802)",
+        category: "gadgets",
+        image: "hero3.jpg"
+    },
+    {
+        id: 2,
+        name: "JBL QuietComfort Ultra Headphones",
+        category: "gadgets",
+        image: "hero4.jpg"
+    },
+    {
+        id: 3,
+        name: "30000mAh Power Bank",
+        category: "gadgets",
+        image: "hero5.jpg"
+    },
+    {
+        id: 4,
+        name: "True Wireless Open-Ear Earbuds",
+        category: "gadgets",
+        image: "hero6.jpg"
+    },
+    {
+        id: 5,
+        name: "Moniepoint POS Machine & Banking Services",
+        category: "services",
+        image: "hero7.jpg"
+    },
+    {
+        id: 6,
+        name: "NIN Enrolment, Modification & Tracking",
+        category: "services",
+        image: "hero8.jpg"
+    },
+    {
+        id: 7,
+        name: "Samsung Galaxy Series (A06, A07, A16)",
+        category: "gadgets",
+        image: "hero9.jpg"
+    },
+    {
+        id: 8,
+        name: "Memory Card",
+        category: "gadgets",
+        image: "hero10.jpg"
+    },
+        {
+        id: 9,
+        name: "Universal router",
+        category: "gadgets",
+        image: "hero11.jpg"
+    },
+            {
+        id: 10,
+        name: "MTN 5G wifi",
+        category: "gadgets",
+        image: "hero12.jpg"
+    },
+            {
+        id: 11,
+        name: "Charger and Cords",
+        category: "gadgets",
+        image: "hero13.jpg"
+    },
+            {
+        id: 12,
+        name: "Flash Drive",
+        category: "gadgets",
+        image: "hero14.jpg"
+    },
+            {
+        id: 13,
+        name: "Any type of Wifi",
+        category: "gadgets",
+        image: "hero15.jpg"
+    },
+            {
+        id: 14,
+        name: "POS SERVICES",
+        category: "sevices",
+        image: "hero16.jpg"
+    },
+            {
+        id: 15,
+        name: "Inverter",
+        category: "gadgets",
+        image: "hero17.jpg"
+    },
+            {
+        id: 16,
+        name: "Making of ID Card",
+        category: "services",
+        image: "hero18.jpg"
+    },
+            {
+        id: 17,
+        name: "Fast charging cords",
+        category: "gadgets",
+        image: "hero19.jpg"
+    },
+            {
+        id: 18,
+        name: "GOTV",
+        category: "gadgets",
+        image: "hero20.jpg"
+    },
+            {
+        id: 19,
+        name: "All Kind Of Headset",
+        category: "gadgets",
+        image: "hero21.jpg"
+    },
+            {
+        id: 20,
+        name: "Charging station",
+        category: "services",
+        image: "hero22.jpg"
+    }
+];
